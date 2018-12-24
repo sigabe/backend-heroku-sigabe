92c3dad9dfbfab62045b4cd2fa43d3da9dce18ad
fahmirevo <fahmi.velosa1997@gmail.com>
2018-12-24 11:37:50 +0700

    reapply third party packages

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -64,10 +64,15 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     'crispy_forms',
+    'django_filters',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
+    'drf_yasg',
 ]
 LOCAL_APPS = [
     'sigabe.users.apps.UsersAppConfig',
